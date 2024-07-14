43358bb76e50aa8f61b5c0c61f9f1e04856596ed
hmuri <113418916+hmuri@users.noreply.github.com>
2024-07-14 15:15:42 +0900

    페이지 라우팅 추가

--- script.js
+++ script.js
@@ -54,23 +54,42 @@
 //   });
 // });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const calculateButton = document.getElementById("calculateButton");
+//   const numInput1 = document.getElementById("numInput1");
+//   const numInput2 = document.getElementById("numInput2");
+//   const messages = document.querySelectorAll(".message");
+
+//   calculateButton.addEventListener("click", function () {
+//     const num1 = parseInt(numInput1.value, 10) || 0;
+//     const num2 = parseInt(numInput2.value, 10) || 0;
+//     let increment = 1;
+
+//     messages.forEach(function (message) {
+//       const adjustedNum1 = num1 + increment;
+//       const adjustedNum2 = num2 + increment;
+//       const result = adjustedNum1 * adjustedNum2;
+//       message.textContent = `${adjustedNum1} X ${adjustedNum2} = ${result}`;
+//       increment++;
+//     });
+//   });
+// });
+
+// function setupHideFunction() {
+//   const closeButton = document.querySelector(".xbox"); //<div class="xbox">X</div>
+//   closeButton.addEventListener("click", function () {
+//     const mainBox = document.querySelector(".mainbox");
+//     mainBox.style.display = "none";
+//   });
+// }
+
+// setupHideFunction();
+
 document.addEventListener("DOMContentLoaded", function () {
-  const calculateButton = document.getElementById("calculateButton");
-  const numInput1 = document.getElementById("numInput1");
-  const numInput2 = document.getElementById("numInput2");
-  const messages = document.querySelectorAll(".message");
+  const toggleButton = document.getElementById("toggleButton"); //document.querySelector("#toggleButton")
+  const mainBox = document.querySelector(".mainbox"); //document.getElementsByClassName("mainbox");
 
-  calculateButton.addEventListener("click", function () {
-    const num1 = parseInt(numInput1.value, 10) || 0;
-    const num2 = parseInt(numInput2.value, 10) || 0;
-    let increment = 1;
-
-    messages.forEach(function (message) {
-      const adjustedNum1 = num1 + increment;
-      const adjustedNum2 = num2 + increment;
-      const result = adjustedNum1 * adjustedNum2;
-      message.textContent = `${adjustedNum1} X ${adjustedNum2} = ${result}`;
-      increment++;
-    });
+  toggleButton.addEventListener("click", function () {
+    window.location.href = "next.html";
   });
 });
